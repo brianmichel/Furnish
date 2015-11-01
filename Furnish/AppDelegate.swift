b81b0b07b8b2da9a8ca83aa66615f6e06e7b1796
Brian Michel <brian.michel@gmail.com>
2015-11-01 11:12:39 -0500

    Initial Commit

---
+++ Furnish/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Furnish
+//
+//  Created by Brian Michel on 11/1/15.
+//  Copyright © 2015 Brian Michel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
