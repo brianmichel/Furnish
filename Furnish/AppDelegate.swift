c436c92f2363c8c27ef263ea62783b4adab0c6b4
Brian Michel <brian.michel@gmail.com>
2015-11-15 13:41:11 -0500

    Removing unused code

--- Furnish/AppDelegate.swift
+++ Furnish/AppDelegate.swift
@@ -13,18 +13,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-    let sim_ctl = SimCtl()
-
-    let queue = NSOperationQueue()
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
-        sim_ctl.start()
-
-//        let task = SimCtlOperations.install(path: "~/Library/Developer/Xcode/DerivedData/Orangina-fxlmalmfvzgjlsbadupjkptfhtmx/Build/Products/Debug-iphonesimulator/Orangina.app") { (task, data: NSMutableData?) -> Void in
-//        }
-//
-//        queue.addOperation(task)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
